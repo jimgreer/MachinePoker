1bdaf78777da390d5646d9bdd5b53b2e8142dd07
Jim Greer <jimgreer@gmail.com>
2017-02-01 15:59:30 -0800

    Play against statusBot

--- examples/game.js
+++ examples/game.js
@@ -1,16 +1,17 @@
 var MachinePoker = require('../lib/index')
     , LocalSeat = MachinePoker.seats.JsLocal
     , RandBot = require('./bots/randBot')
+    , Status3Bot = require('./bots/status3Bot')
     , HumanBot = require('./bots/humanBot')
     , narrator = MachinePoker.observers.narrator
     , fileLogger = MachinePoker.observers.fileLogger('./examples/results.json');
 
 var table = MachinePoker.create({
-  maxRounds: 1
+  maxRounds: 10
 });
 
 var players = [
-  LocalSeat.create(RandBot),
+  LocalSeat.create(Status3Bot),
   LocalSeat.create(HumanBot)
 ];
 table.addPlayers(players);
